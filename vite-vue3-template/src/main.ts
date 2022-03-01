2d36abb42ac977b50fd5d4f74369d6a18f88e31a
liuminxia <liuminxia@tal.com>
2022-03-01 09:54:22 +0800

    feat: 配置 Axios

--- vite-vue3-template/src/main.ts
+++ vite-vue3-template/src/main.ts
@@ -4,6 +4,6 @@ import router from "./router/index";
 import store from "./store/index";
 
 import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+// import "element-plus/lib/theme-chalk/index.css";
 
 createApp(App).use(router).use(store).use(ElementPlus).mount("#app");
